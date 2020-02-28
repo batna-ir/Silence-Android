4d04c5d0828b37121d598b853654dd6b3126b870
javad <javad.aboutorabi@gmail.com>
2020-02-28 20:30:20 +0400

    - Import messages when secure SMS is the default SMS app

--- src/org/smssecure/smssecure/components/reminder/SystemSmsImportReminder.java
+++ src/org/smssecure/smssecure/components/reminder/SystemSmsImportReminder.java
@@ -2,46 +2,72 @@ package org.smssecure.smssecure.components.reminder;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import org.smssecure.smssecure.ConversationListActivity;
 import org.smssecure.smssecure.DatabaseMigrationActivity;
 import org.smssecure.smssecure.R;
-import org.smssecure.smssecure.service.ApplicationMigrationService;
 import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.service.ApplicationMigrationService;
+
+import java.util.Objects;
+
+import static android.provider.Telephony.Sms.getDefaultSmsPackage;
 
 public class SystemSmsImportReminder extends Reminder {
 
+  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   public SystemSmsImportReminder(final Context context, final MasterSecret masterSecret) {
     super(context.getString(R.string.reminder_header_sms_import_title),
           context.getString(R.string.reminder_header_sms_import_text),
           context.getString(R.string.reminder_header_sms_import_button));
 
-    final OnClickListener okListener = new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(context, ApplicationMigrationService.class);
-        intent.setAction(ApplicationMigrationService.MIGRATE_DATABASE);
-        intent.putExtra("master_secret", masterSecret);
-        context.startService(intent);
 
-        Intent nextIntent = new Intent(context, ConversationListActivity.class);
-        intent.putExtra("master_secret", masterSecret);
+    if (Objects.equals(getDefaultSmsPackage(context),context.getPackageName())){
+      Intent intent = new Intent(context, ApplicationMigrationService.class);
+      intent.setAction(ApplicationMigrationService.MIGRATE_DATABASE);
+      intent.putExtra("master_secret", masterSecret);
+      context.startService(intent);
 
-        Intent activityIntent = new Intent(context, DatabaseMigrationActivity.class);
-        activityIntent.putExtra("master_secret", masterSecret);
-        activityIntent.putExtra("next_intent", nextIntent);
-        context.startActivity(activityIntent);
-      }
-    };
+      Intent nextIntent = new Intent(context, ConversationListActivity.class);
+      intent.putExtra("master_secret", masterSecret);
+
+      Intent activityIntent = new Intent(context, DatabaseMigrationActivity.class);
+      activityIntent.putExtra("master_secret", masterSecret);
+      activityIntent.putExtra("next_intent", nextIntent);
+      context.startActivity(activityIntent);
+    }
+
+
+//    final OnClickListener okListener = new OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        Intent intent = new Intent(context, ApplicationMigrationService.class);
+//        intent.setAction(ApplicationMigrationService.MIGRATE_DATABASE);
+//        intent.putExtra("master_secret", masterSecret);
+//        context.startService(intent);
+//
+//        Intent nextIntent = new Intent(context, ConversationListActivity.class);
+//        intent.putExtra("master_secret", masterSecret);
+//
+//        Intent activityIntent = new Intent(context, DatabaseMigrationActivity.class);
+//        activityIntent.putExtra("master_secret", masterSecret);
+//        activityIntent.putExtra("next_intent", nextIntent);
+//        context.startActivity(activityIntent);
+//      }
+//    };
+
+
     final OnClickListener cancelListener = new OnClickListener() {
       @Override
       public void onClick(View v) {
         ApplicationMigrationService.setDatabaseImported(context);
       }
     };
-    setOkListener(okListener);
+//    setOkListener(okListener);
     setDismissListener(cancelListener);
   }
 
