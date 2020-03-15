1e12c0ab2c053fdde3642b8b83b790e6d35a32b7
javad <javad.aboutorabi@gmail.com>
2020-03-15 12:08:22 +0400

    - When app crashed don't clear database

--- src/saba/AppManager.java
+++ src/saba/AppManager.java
@@ -1,13 +1,7 @@
 package saba;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-
-import org.smssecure.smssecure.ConversationListActivity;
-import org.smssecure.smssecure.R;
 
 import java.io.File;
 
@@ -15,30 +9,31 @@ public class AppManager {
 
 
     public static void clearData(Context context, AppCompatActivity appCompatActivity) {
-
-        try {
-            new Handler().post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(context, context.getString(R.string.reconfiguring_app), Toast.LENGTH_LONG).show();
-                }
-            });
-            deleteCache(context);
-            deletePref();
-            deleteDbs(context);
-            appCompatActivity.finish();
-            context.startActivity(new Intent(context, ConversationListActivity.class));
-        } catch (Exception e) {
-            e.printStackTrace();
-            try {
-                // clearing app data the hard way!
-                Runtime runtime = Runtime.getRuntime();
-                runtime.exec("pm clear com.sabaos.secureSMS");
-
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        }
+        return;
+//
+//        try {
+//            new Handler().post(new Runnable() {
+//                @Override
+//                public void run() {
+//                    Toast.makeText(context, context.getString(R.string.reconfiguring_app), Toast.LENGTH_LONG).show();
+//                }
+//            });
+//            deleteCache(context);
+//            deletePref();
+//            deleteDbs(context);
+//            appCompatActivity.finish();
+//            context.startActivity(new Intent(context, ConversationListActivity.class));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            try {
+//                // clearing app data the hard way!
+//                Runtime runtime = Runtime.getRuntime();
+//                runtime.exec("pm clear com.sabaos.secureSMS");
+//
+//            } catch (Exception e1) {
+//                e1.printStackTrace();
+//            }
+//        }
     }
 
     private static void deleteCache(Context context) {
